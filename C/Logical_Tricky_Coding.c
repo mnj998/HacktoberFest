6964546487edf69cefa7d1e2ec4c37137f23a658
Dattha Prasad <57026609+datthaprasad@users.noreply.github.com>
2020-10-21 16:02:31 +0530

    Add files via upload

---
+++ C/Logical_Tricky_Coding.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void sum(int num1,int num2){
+    //write a program to print sum of 2 numbers without using '+' operater and print here.
+}
+
+void swap(int num1,int num2){
+    //Complete this program to swap 2 numbers without using third (temp) variable and print here.
+}
+
+void isEqual(int num1,int num2){
+    //Complete this program to check 2 numbers are equal or not without using Arithematic operators or comparision operators and print the values.
+}
+
+void Devide(int num1,int num2){
+    //complete to devide num1 by num2 without using '/' and print the values.
+}
+
+HellowWorld(){
+    //print Hello World without using ';'
+}
+
+
+int main(){
+    sum(1,2);
+    swap(10,20);
+    isEqual(10,20);
+    Devide(20,10);
+    HellowWorld();
+    return 0;
+}
