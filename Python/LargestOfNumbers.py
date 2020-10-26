ccccbfcd73b1a00a81f332cf23b6eedbeac32c15
Manoj krshn <manojkrishna129@gmail.com>
2020-10-26 17:41:52 +0530

    added 3 largest of numbers

--- Python/LargestOfNumbers.py
+++ Python/LargestOfNumbers.py
@@ -6,6 +6,7 @@ def largestOfTwo(num1, num2):
 
 def largestOfThree(num1, num2, num3):
     # Complete the function
+    print(max(n1, n2, n3))
     return largestOfTwo( largestOfTwo( num1, num2), num3 )
 
 def largestOfFour( num1, num2, num3, num4 ):
@@ -22,12 +23,12 @@ n3 = -10
 n4 = 500
 n5 = -50
 
-print("Largest among n1, n2 : ", largestOfTwo(n1, n2))
+#print("Largest among n1, n2 : ", largestOfTwo(n1, n2))
 
 """
 Please create below functions to find the largest among the numbers 
 """
 
 print( "Largest among n1, n2, n3 : ", largestOfThree( n1, n2, n3 ) ) 
-print( "Largest among n1, n2, n3, n4 : ", largestOfFour( n1, n2, n3, n4 ) ) 
-print( "Largest among n1, n2, n3, n4, n5 : ", largestOfFive( n1, n2, n3, n4, n5 ) ) 
+#print( "Largest among n1, n2, n3, n4 : ", largestOfFour( n1, n2, n3, n4 ) ) 
+#print( "Largest among n1, n2, n3, n4, n5 : ", largestOfFive( n1, n2, n3, n4, n5 ) ) 
