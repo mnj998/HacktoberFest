ec98c7adadd3cccf598213584f06134b18d6b8ca
Manoj krshn <manojkrishna129@gmail.com>
2020-10-29 17:13:44 +0530

    expression to accept str ending with alphanumeric character

---
+++ Python/Regex.py
@@ -0,0 +1,41 @@
+# Python program to accept string ending 
+# with only alphanumeric character.  
+  
+# re module provides support 
+# for regular expressions 
+import re 
+  
+# Make a regular expression to accept string 
+# ending with alphanumeric character 
+--------------
+      
+# Define a function for accepting string 
+# ending with alphanumeric character 
+def check(string):  
+  
+     # pass the regualar expression 
+     # and the string in search() method 
+    if(re.search(regex, string)):  
+        print("Accept")  
+          
+    else:  
+        print("Discard")  
+      
+  
+# Driver Code  
+if __name__ == '__main__' :  
+      
+    # Enter the string  
+    string = "ankirai@"
+      
+    # calling run function  
+    check(string) 
+  
+    string = "ankitrai326"
+    check(string) 
+  
+    string = "ankit."
+    check(string) 
+  
+    string = "geeksforgeeks"
+    check(string) 
