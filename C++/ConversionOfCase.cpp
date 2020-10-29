6ac5106c7090cbcb63f47a30b6dd0a85e2df2c7d
Manoj krshn <manojkrishna129@gmail.com>
2020-10-29 21:55:42 +0530

    Conversion

---
+++ C++/ConversionOfCase.cpp
@@ -0,0 +1,15 @@
+#include <cwctype> 
+#include <iostream> 
+using namespace std; 
+
+int main() {   
+	wchar_t rs1 = 'H'; 
+	wchar_t rs2 = 'e'; 
+	wchar_t rs3 = 'l';
+	wchar_t rs4 = 'P';
+
+	// Write a Function to check if the character 
+	// is a lowercase character or not 
+	// using if-else
+	
+}
