76a18c98fe4c25de94b3f59864322fb31287ba68
Sasuke-003 <56964273+Sasuke-003@users.noreply.github.com>
2020-10-29 14:34:54 +0530

    Update BinarySearch.java
    added recursive binary search method in java

--- Algorithms/BinarySearch/BinarySearch.java
+++ Algorithms/BinarySearch/BinarySearch.java
@@ -28,7 +28,30 @@ class BinarySearch {
      int RecursiveBinarySearch(int arr[], int l, int r, int x) 
     { 
 		//write the code to implement Recursive BinarySearch
-		return 0;
+		 //Restrict the boundary of right index 
+        // and the left index to prevent 
+        // overflow of indices. 
+        if (r >= l && l<arr.length-1) { 
+           
+            int mid = l + (r - l) / 2;
+ 
+            // If the element is present 
+            // at the middle itself
+            if (arr[mid] == x)
+                return mid;
+ 
+            // If element is smaller than mid, then it can only
+            // be present in left subarray
+            if (arr[mid] > x)
+                return binarySearch(arr, l, mid - 1, x);
+ 
+            // Else the element can only be present in right
+            // subarray
+            return binarySearch(arr, mid + 1, r, x);
+        }
+ 
+        // We reach here when element is not present in array
+        return -1;
     }
 
 	public static void main(String args[]) 
