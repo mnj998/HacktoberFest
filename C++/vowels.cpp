caf94123fea3f9143a18a026048fc3105c614051
Dheeraj J Rao <dheerajraoj@gmail.com>
2020-10-30 23:09:20 +0530

    Issues fixed

--- C++/vowels.cpp
+++ C++/vowels.cpp
@@ -9,8 +9,18 @@ int main() {
     fgets(line, sizeof(line), stdin);
 
     for (int i = 0; line[i] != '\0'; ++i) {
-        //complete the statement to find out the vowels and consonants
-        //using if
+        if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' ||
+            line[i] == 'o' || line[i] == 'u' || line[i] == 'A' ||
+            line[i] == 'E' || line[i] == 'I' || line[i] == 'O' ||
+            line[i] == 'U') {
+            ++vowels;
+        } else if ((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z')) {
+            ++consonant;
+        } else if (line[i] >= '0' && line[i] <= '9') {
+            ++digit;
+        } else if (line[i] == ' ') {
+            ++space;
+        }
     }
 
     printf("Vowels: %d", vowels);
