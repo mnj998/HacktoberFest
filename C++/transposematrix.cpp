bcb11787da9d54bb7cbff445a0507b00c854ba79
Dheeraj J Rao <dheerajraoj@gmail.com>
2020-10-30 23:19:22 +0530

    Issues solved

--- C++/transposematrix.cpp
+++ C++/transposematrix.cpp
@@ -21,13 +21,19 @@ int main() {
                 printf("\n");
         }
 
-    // Find the transpose of matrix a
--------------------------------
-------------------------------
+    // Finding the transpose of matrix a
+    for (i = 0; i < r; ++i)
+        for (j = 0; j < c; ++j) {
+            transpose[j][i] = a[i][j];
+        }
 
-    // Display the transpose of matrix a
+    // Displaying the transpose of matrix a
     printf("\nTranspose of the matrix:\n");
-    ------------
-    -------------
+    for (i = 0; i < c; ++i)
+        for (j = 0; j < r; ++j) {
+            printf("%d  ", transpose[i][j]);
+            if (j == r - 1)
+                printf("\n");
+        }
     return 0;
 }
